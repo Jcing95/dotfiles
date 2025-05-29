55ca65cca2523efbb815073307650b3e3d1ce699
Jcing <dev@jcing.de>
2025-05-29 20:14:23 +0200

    run custom ps1 in profile

--- profile.ps1
+++ profile.ps1
@@ -4,7 +4,13 @@ function ws {
     Set-Location 'D:\workspace'
 }
 
+function conf {
+  nvim ~\.config\
+}
+
 $PSStyle.FileInfo.Directory = "`e[48;2;21;56;16m"
 
 Set-PsFzfOption -PSReadlineChordProvider 'Ctrl+t' -PSReadlineChordReverseHistory 'Ctrl+r'
+
+& ~\.config\custom.ps1
 
