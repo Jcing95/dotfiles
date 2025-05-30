17ddd407d433919e418b11ed2cf1375a2c4664ec
Jcing <dev@jcing.de>
2025-05-30 10:43:00 +0200

    add wezterm keybinding file

--- install.ps1
+++ install.ps1
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+
+#TODO:
+# create symlinks for:
+# - nvim conf on win (LocalAppData/nvim)
+# - profile.ps1 $PROFILE
+# - get env from MSVC (somehow)
+# install:
+# - wget
+# - fzf
+# - fd
+# -
+
+
 : '<#
 '
 echo "Running bash installer..."
--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,3 +1,6 @@
+--load keybindings
+require 'keys'
+
 -- Pull in the wezterm API
 local wezterm = require 'wezterm'
 
@@ -45,24 +48,7 @@ config.macos_window_background_blur = 10
 config.send_composed_key_when_left_alt_is_pressed = true
 config.leader = { key='b', mods='CTRL' }
 config.disable_default_key_bindings = true
-config.keys = {
-  { key = "b", mods = "LEADER|CTRL",  action=wezterm.action{SendString="\x02"}},
-  { key ="v",  mods="SHIFT|CTRL",    action=wezterm.action.PasteFrom 'Clipboard'},
-  { key ="c",  mods="SHIFT|CTRL",    action=wezterm.action.CopyTo 'Clipboard'},
-  { key = '*', mods='SHIFT|CTRL', action ='IncreaseFontSize' },
-  { key = '_', mods='SHIFT|CTRL', action ='DecreaseFontSize' },
-  { key = '=', mods='SHIFT|CTRL', action ='ResetFontSize' },
-  { key = '%', mods='LEADER|SHIFT', action =wezterm.action{SplitVertical={domain='CurrentPaneDomain'}}},
-  { key = '"', mods='LEADER|SHIFT', action =wezterm.action{SplitHorizontal={domain='CurrentPaneDomain'}}},
-  { key = "h", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Left"}},
-  { key = "j", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Down"}},
-  { key = "k", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Up"}},
-  { key = "l", mods = "LEADER", action=wezterm.action{ActivatePaneDirection="Right"}},
-  { key = "LeftArrow", mods = "CTRL|SHIFT", action=wezterm.action{AdjustPaneSize={"Left", 2}}},
-  { key = "DownArrow", mods = "CTRL|SHIFT", action=wezterm.action{AdjustPaneSize={"Down", 2}}},
-  { key = "UpArrow", mods = "CTRL|SHIFT", action=wezterm.action{AdjustPaneSize={"Up", 2}}},
-  { key = "RightArrow", mods = "CTRL|SHIFT", action=wezterm.action{AdjustPaneSize={"Right", 2}}},
-}
+
 
 -- and finally, return the configuration to wezterm
 return config
