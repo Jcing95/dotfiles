05857bd6bd37b4553b67dec2733bcdb32bcc6183
Jasin Aferkou <dev@jcing.de>
2026-01-07 10:11:41 +0100

    rename installer to install-nix

---
+++ install-nix.sh
@@ -0,0 +1,12 @@
+#! /bin/sh
+
+dir="$(cd "$(dirname "$0")" && pwd)"
+mkdir -p ~/.config
+mkdir -p ~/.config/Nextcloud
+mkdir -p ~/.config/hypr/
+ln -sfn $dir/wezterm ~/.config/wezterm
+ln -sfn $dir/waybar ~/.config/waybar
+ln -sfn $dir/lazyvim ~/.config/nvim
+ln -sfn $dir/nextcloud.cfg ~/.config/Nextcloud
+ln -sfn $dir/hyprland.conf ~/.config/hypr/hyprland.conf
+ln -sfn $dir/hyprlock.conf ~/.config/hypr/hyprlock.conf
