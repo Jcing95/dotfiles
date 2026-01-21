c650cb7a71d82c083ec6b537e2887cd7168dca1d
Jcing <dev@jcing.de>
2026-01-21 23:01:16 +0100

    use remote color jcingdark nvim theme + update tuigreet theme

--- lazyvim/lua/plugins/jcing-dark.lua
+++ lazyvim/lua/plugins/jcing-dark.lua
@@ -1,9 +1,9 @@
 return {
-  "jcing-dark.nvim",
-  dev = true,
+  "jcing95/jcing-dark.nvim",
   lazy = false,
+  priority = 1000,
   config = function()
-    require("jcing-dark").setup()
+    require("jcing-dark").setup({})
     vim.cmd.colorscheme("jcing-dark")
   end,
 }
