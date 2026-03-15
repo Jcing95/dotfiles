aaabfff0d04d0f5d7802f2636bc2d27649b1e301
Jcing <dev@jcing.de>
2026-03-15 13:09:16 +0100

    add laptop lid conf

--- nix/hosts/laptop/default.nix
+++ nix/hosts/laptop/default.nix
@@ -12,6 +12,13 @@
 
   networking.hostName = "laptop";
 
+  # Allow laptop to stay on with lid closed (connected to external display)
+  services.logind.settings.Login = {
+    HandleLidSwitch = "ignore";
+    HandleLidSwitchDocked = "ignore";
+    HandleLidSwitchExternalPower = "ignore";
+  };
+
   # Windows dual-boot: set Windows as default boot option
   # boot.loader.systemd-boot.extraInstallCommands = ''
   #   ${pkgs.gnused}/bin/sed -i 's/^default .*/default auto-windows/' /boot/loader/loader.conf
