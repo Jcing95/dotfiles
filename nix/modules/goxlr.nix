b8705f6bbce2c5fb42bada99dc25feb8adf8acd4
Jcing <dev@jcing.de>
2026-03-17 09:09:21 +0100

    fix goxlr

--- nix/modules/goxlr.nix
+++ nix/modules/goxlr.nix
@@ -2,23 +2,37 @@
 { config, pkgs, ... }:
 
 {
-  
+
   services.goxlr-utility = {
     enable = true;
-    autoStart.xdg = true;
+    autoStart.xdg = false;  # Disable XDG autostart, we use systemd instead
   };
-  
+
   systemd.user.services = {
+    # Custom systemd service for goxlr-daemon (since the module only supports XDG autostart)
+    goxlr-daemon = {
+      description = "GoXLR Utility Daemon";
+      after = [ "pipewire.service" "pipewire-pulse.service" ];
+      wants = [ "pipewire.service" ];
+      wantedBy = [ "default.target" ];
+      serviceConfig = {
+        ExecStart = "${pkgs.goxlr-utility}/bin/goxlr-daemon";
+        Restart = "on-failure";
+        RestartSec = 5;
+      };
+    };
+
     goxlr-set-default = {
       description = "Set GoXLR System as default sink";
-      after = [ 
+      after = [
         "pipewire-pulse.service"
-        "goxlr-utility.service"
+        "goxlr-daemon.service"
       ];
-      requires = [ "goxlr-utility.service" ];
-      wantedBy = [ "graphical-session.target" ];
+      requires = [ "goxlr-daemon.service" ];
+      wantedBy = [ "default.target" ];
       serviceConfig = {
         Type = "oneshot";
+        ExecStartPre = "${pkgs.coreutils}/bin/sleep 2";  # Give daemon time to register sinks
         ExecStart = "${pkgs.pulseaudio}/bin/pactl set-default-sink alsa_output.usb-TC-Helicon_GoXLRMini-00.HiFi__Speaker__sink";
         RemainAfterExit = true;
       };
