73ea9da14e4b75da95ce6e3cce8780b2788513c2
Jcing <dev@jcing.de>
2026-07-22 10:54:03 +0200

    fix nvim + cleanup opencode

--- lazyvim/lua/plugins/snacks.lua
+++ lazyvim/lua/plugins/snacks.lua
@@ -6,6 +6,7 @@ return {
       sources = {
         explorer = {
           hidden = true, -- show dotfiles in the file tree
+          ignored = true, -- show gitignored files in the file tree
           win = {
             list = {
               keys = {
