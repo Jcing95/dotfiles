1332c916e0f66c842a124ba49093c5b589e4b136
Jcing <dev@jcing.de>
2026-08-06 21:22:53 +0200

    enable hdr on workstation

--- hypr/workstation.lua
+++ hypr/workstation.lua
@@ -1,16 +1,23 @@
-hl.monitor({ output = "desc:Samsung Electric Company Odyssey G95NC HNTWB00204",
-             mode = "highres@120", position = "auto", scale = 1, vrr = 1 })
+hl.monitor({
+	output = "desc:Samsung Electric Company Odyssey G95NC HNTWB00204",
+	mode = "highres@highrr",
+	position = "auto",
+	scale = 1,
+	vrr = 1,
+	bitdepth = 10,
+	cm = "auto",
+})
 hl.monitor({ output = "", mode = "preferred", position = "auto-center-down", scale = 1 })
 
 hl.env("HYPRCURSOR_THEME", "Nordzy-hyprcursors-white")
-hl.env("HYPRCURSOR_SIZE",  "36")
-hl.env("XCURSOR_THEME",    "Nordzy-cursors-white")
-hl.env("XCURSOR_SIZE",     "36")
+hl.env("HYPRCURSOR_SIZE", "36")
+hl.env("XCURSOR_THEME", "Nordzy-cursors-white")
+hl.env("XCURSOR_SIZE", "36")
 
 hl.config({
-    misc = { vrr = 1 },
+	misc = { vrr = 1 },
 })
 
 hl.on("hyprland.start", function()
-    hl.exec_cmd("waybar")
+	hl.exec_cmd("waybar")
 end)
